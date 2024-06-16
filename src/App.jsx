fed6050783bf8e6a06d5c92c3607ab74ab27b2b4
Prashant <hadaprashant2002@gmail.com>
2024-06-16 17:39:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-gray-800 text-white'>
+      <Navbar/>
+    </div>
+    
+  )
+}
+
+export default App
