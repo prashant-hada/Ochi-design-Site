9da23827f55cb9eec4093484d2e41e116b8609b3
Prashant <hadaprashant2002@gmail.com>
2024-09-10 00:07:20 +0530

    added featured card tags

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,6 +1,6 @@
 import { motion, useAnimation } from 'framer-motion'
 import React from 'react'
-import FeaturedProjectCard from './FeaturedProjectCard'
+import FeatureCardTags from './FeatureCardTags'
 
 function Featured() {
    
@@ -30,7 +30,7 @@ function Featured() {
                 <img className=' w-full h-full bg-cover transition duration-1000 ease-in-out hover:transform hover:scale-[115%] hover:delay-300' src="/Ochi-design-Site/images/card-image1.png" alt="card image" />
               </div>
             </motion.div>
-            {/* <FeaturedProjectCard text="fyde" cardIndex={0} /> */}
+            <FeatureCardTags name={["audit", "copywriting", "sales deck", "slide design"]}/>
             </div>
             <div className="">
             <ul className=' list-disc list-inside'>
@@ -44,6 +44,8 @@ function Featured() {
               <img className=' w-full h-full bg-cover transition duration-1000 ease-in-out hover:transform hover:scale-[115%] hover:delay-300' src="/Ochi-design-Site/images/card-image2.png" alt="card image" />
               </div>
             </motion.div>
+            <FeatureCardTags name={["Agency","company presentation"]}/>
+
             </div>
           </div>
           <div className="cards w-full flex gap-5 mt-16">
@@ -52,26 +54,28 @@ function Featured() {
                 <li className='text-black text-[1.5vw] font-medium uppercase'>Trawa</li>
               </ul>
             <motion.div onHoverStart={()=>handleHoverStart(2)} onHoverEnd={()=>handleHoverEnd(2)} className="card-container relative w-full h-[38vw] rounded-3xl">
-            <h1 className='absolute flex overflow-hidden left-full top-1/2 -translate-x-1/2 -translate-y-1/2 z-[9] text-8xl text-[#9194E0] font-bold leading-none tracking-tight uppercase'>
+            <h1 className='absolute flex overflow-hidden left-full top-1/2 -translate-x-1/2 -translate-y-1/2 z-[9] text-8xl text-[#c0c2f2] font-bold leading-none tracking-tight uppercase'>
             {"TRAWA".split('').map((item,index)=><motion.span initial={{y:"100%"}} animate={cards[2]} transition={{ease:[0.22, 1, 0.36, 1], delay:index*0.025} } className='inline-block'>{item}</motion.span>)}
             </h1>
               <div className="card w-full h-full rounded-xl overflow-hidden transition duration-1000 ease-in-out hover:transform hover:scale-[95%]">
                 <img className=' w-full h-full bg-cover transition duration-1000 ease-in-out hover:transform hover:scale-[115%] hover:delay-300' src="/Ochi-design-Site/images/card-image3.png" alt="card image" />
               </div>
             </motion.div>
+            <FeatureCardTags name={["brand identity","design research", "investor deck"]}/>
             </div>
             <div className="">
             <ul className=' list-disc list-inside'>
                 <li className='text-black text-[1.5vw] font-medium uppercase'>PREMIUM BLEND</li>
               </ul>
             <motion.div onHoverStart={()=>handleHoverStart(3)} onHoverEnd={()=>handleHoverEnd(3)} className="card-container relative w-full h-[38vw] rounded-3xl">
-            <h1 className='absolute flex overflow-hidden right-full top-1/2 translate-x-1/2 -translate-y-1/2 z-[9] text-8xl text-[#9194E0] font-bold leading-none tracking-tight uppercase'>
+            <h1 className='absolute flex overflow-hidden right-full top-1/2 translate-x-1/2 -translate-y-1/2 z-[9] text-8xl text-[#c0c2f2] font-bold leading-none tracking-tight uppercase'>
             {"PREMIUM BLEND".split('').map((item,index)=><motion.span initial={{y:"100%"}} animate={cards[3]} transition={{ease:[0.22, 1, 0.36, 1], delay:index*0.025} } className='inline-block'>{item}</motion.span>)}
             </h1>
               <div className="card w-full h-full rounded-xl overflow-hidden transition duration-1000 ease-in-out hover:transform hover:scale-[95%]">
               <img className=' w-full h-full bg-cover transition duration-1000 ease-in-out hover:transform hover:scale-[115%] hover:delay-300' src="/Ochi-design-Site/images/card-image4.png" alt="card image" />
               </div>
             </motion.div>
+            <FeatureCardTags name={["Branded template"]}/>
             </div>
           </div>
           <div className="cards w-full flex gap-5 mt-16">
@@ -80,26 +84,28 @@ function Featured() {
                 <li className='text-black text-[1.5vw] font-medium uppercase'>CARDBOARD SPACESHIP</li>
               </ul>
             <motion.div onHoverStart={()=>handleHoverStart(4)} onHoverEnd={()=>handleHoverEnd(4)} className="card-container relative w-full h-[38vw] rounded-3xl">
-            <h1 className='absolute flex overflow-hidden left-full top-1/2 -translate-x-1/2 -translate-y-1/2 z-[9] text-8xl text-[#9194E0] font-bold leading-none tracking-tight uppercase'>
+            <h1 className='absolute flex overflow-hidden left-full top-1/2 -translate-x-1/2 -translate-y-1/2 z-[9] text-8xl text-[#c0c2f2] font-bold leading-none tracking-tight uppercase'>
             {"CARDBOARD SPACESHIP".split('').map((item,index)=><motion.span initial={{y:"100%"}} animate={cards[4]} transition={{ease:[0.22, 1, 0.36, 1], delay:index*0.025} } className='inline-block'>{item}</motion.span>)}
             </h1>
               <div className="card w-full h-full rounded-xl overflow-hidden transition duration-1000 ease-in-out hover:transform hover:scale-[95%]">
                 <img className=' w-full h-full bg-cover transition duration-1000 ease-in-out hover:transform hover:scale-[115%] hover:delay-300' src="/Ochi-design-Site/images/card-image5.png" alt="card image" />
               </div>
             </motion.div>
+            <FeatureCardTags name={["branded template","sales deck","Social Media templates"]}/>
             </div>
             <div className="">
             <ul className=' list-disc list-inside'>
                 <li className='text-black text-[1.5vw] font-medium uppercase'>AH2 & Matt Horn</li>
               </ul>
             <motion.div onHoverStart={()=>handleHoverStart(5)} onHoverEnd={()=>handleHoverEnd(5)} className="card-container relative w-full h-[38vw] rounded-3xl">
-            <h1 className='absolute flex overflow-hidden right-full top-1/2 translate-x-1/2 -translate-y-1/2 z-[9] text-8xl text-[#9194E0] font-bold leading-none tracking-tight uppercase'>
+            <h1 className='absolute flex overflow-hidden right-full top-1/2 translate-x-1/2 -translate-y-1/2 z-[9] text-8xl text-[#c0c2f2] font-bold leading-none tracking-tight uppercase'>
             {"AH2 & Matt Horn".split('').map((item,index)=><motion.span initial={{y:"100%"}} animate={cards[5]} transition={{ease:[0.22, 1, 0.36, 1], delay:index*0.025} } className='inline-block'>{item}</motion.span>)}
             </h1>
               <div className="card w-full h-full rounded-xl overflow-hidden transition duration-1000 ease-in-out hover:transform hover:scale-[95%]">
               <img className=' w-full h-full bg-cover transition duration-1000 ease-in-out hover:transform hover:scale-[115%] hover:delay-300' src="/Ochi-design-Site/images/card-image6.png" alt="card image" />
               </div>
             </motion.div>
+            <FeatureCardTags name={["pitch deck"]}/>
             </div>
           </div>
         </div>
