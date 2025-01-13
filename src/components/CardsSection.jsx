429effe63a31068f4dacdb4fc591118167dc280b
Prashant <hadaprashant2002@gmail.com>
2025-01-13 14:08:55 +0530

    updated cards section

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 export default function () {
   return (
-    <div className='w-full h-screen flex items-center bg-zinc-50 px-10 gap-5'>
-      <div className="cards-container w-1/2 h-[57vh] ">
+    <div className='w-full h-screen flex flex-col md:flex-row items-center bg-zinc-50 px-10 gap-5'>
+      <div className="cards-container w-full md:w-1/2 h-[57vh] ">
         <div className="card relative rounded-xl w-full h-full  bg-[#004D43] flex  items-center justify-center">
           <img className='w-40' src="/Ochi-design-Site/images/logo001.svg" alt="LOGO"/>
           <button className='absolute left-8 bottom-8 px-2 py-1 border-2 rounded-full text-[#CDea68] border-[#CDea68]'>&copy; 2019-2022</button>
         </div>
       </div>
-      <div className="cards-container w-1/2 h-[57vh] flex items- gap-5 ">
+      <div className="cards-container w-full md:w-1/2 h-[57vh] flex items- gap-5 ">
         <div className="card relative rounded-xl w-1/2 h-full bg-[#212121] flex items-center justify-center">
           <img className='w-40' src="/Ochi-design-Site/images/logo002.svg" alt="LOGO"/>
-          <button className='absolute left-8 bottom-8 px-2 py-1 border-2 uppercase rounded-full text-[#fff] border-[#fff]'>rating 5.0 on clutch</button>
+          <button className='absolute md:left-8 bottom-8 px-2 py-1 border-2 uppercase rounded-full text-[#fff] border-[#fff]'>rating 5.0 on clutch</button>
         </div>
         <div className="card relative rounded-xl w-1/2 h-full bg-[#212121] flex items-center justify-center">
         <img className='w-28' src="/Ochi-design-Site/images/logo003.png" alt="LOGO"/>
-          <button className='absolute left-8 bottom-8 px-2 py-1 border-2 uppercase rounded-full text-[#fff] border-[#fff]'>busines bootcamp alumini</button>
+          <button className='absolute md:left-8 bottom-8 px-2 py-1 border-2 uppercase rounded-full text-[#fff] border-[#fff]'>busines bootcamp alumini</button>
           </div>
       </div>
     </div>
